928e7350649a2ad7cb617e40f07792fdf6f6415a
krista palmer <krista@example.com>
2012-11-12 13:29:33 -0500

    test file

---
+++ src/test/java/com/TableFlip/SpaceTrader/Model/ShipTest.java
@@ -0,0 +1,30 @@
+package com.TableFlip.SpaceTrader.Model;
+
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kpalmer
+ * Date: 11/12/12
+ * Time: 1:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ShipTest extends TestCase {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testFly() throws Exception {
+
+    }
+}
